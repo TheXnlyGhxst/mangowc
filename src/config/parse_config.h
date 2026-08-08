9a9716fbe892306d2b30c08a004bf1f984428491
DreamMaoMao <2523610504@qq.com>
2026-08-08 10:21:51 +0800

    feat: simple touch support

--- src/config/parse_config.h
+++ src/config/parse_config.h
@@ -343,6 +343,9 @@ typedef struct {
 	int32_t drag_lock;
 	uint32_t button_map;
 
+	/* touch */
+	int32_t touch_enable_mouse_emulation;
+
 	/* appearance */
 	int32_t smartgaps;
 	uint32_t gappih;
@@ -1857,6 +1860,8 @@ bool parse_option(Config *config, char *key, char *value, int line_number) {
 		config->repeat_delay = atoi(value);
 	} else if (strcmp(key, "disable_trackpad") == 0) {
 		config->disable_trackpad = atoi(value);
+	} else if (strcmp(key, "touch_enable_mouse_emulation") == 0) {
+		config->touch_enable_mouse_emulation = atoi(value);
 	} else if (strcmp(key, "tap_to_click") == 0) {
 		config->tap_to_click = atoi(value);
 	} else if (strcmp(key, "tap_and_drag") == 0) {
@@ -3920,6 +3925,8 @@ void override_config(void) {
 	config.repeat_delay = CLAMP_INT(config.repeat_delay, 1, 20000);
 	config.numlockon = CLAMP_INT(config.numlockon, 0, 1);
 	config.disable_trackpad = CLAMP_INT(config.disable_trackpad, 0, 1);
+	config.touch_enable_mouse_emulation =
+		CLAMP_INT(config.touch_enable_mouse_emulation, 0, 1);
 	config.tap_to_click = CLAMP_INT(config.tap_to_click, 0, 1);
 	config.tap_and_drag = CLAMP_INT(config.tap_and_drag, 0, 1);
 	config.drag_lock = CLAMP_INT(config.drag_lock, 0, 1);
@@ -4085,6 +4092,7 @@ void set_value_default() {
 	config.repeat_delay = 600;
 
 	config.disable_trackpad = 0;
+	config.touch_enable_mouse_emulation = 1;
 	config.tap_to_click = 1;
 	config.tap_and_drag = 1;
 	config.drag_lock = 1;
