e6d4a54923f91386991f615838ea91f68456c6ef
DreamMaoMao <2523610504@qq.com>
2026-08-12 11:52:03 +0800

    fix: miss reset isminimize state

--- src/mango.c
+++ src/mango.c
@@ -6212,6 +6212,7 @@ void show_hide_client(Client *c) {
 		tag_client(&(Arg){.ui = target}, c);
 	} else {
 		c->tags = c->oldtags;
+		c->isminimized = 0;
 		if (c->mon)
 			arrange(c->mon, false, false);
 	}
